518e2c33191fd41c1f7a7238f712b69037dc29fc
dhara-maru <dharamaru406@gmail.com>
2024-08-30 19:13:53 +0530

    new file

---
+++ sum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class sum{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter num1: ");
+        int n1 = sc.nextInt();
+        System.out.println("enter num2: ");
+        int n2 = sc.nextInt();
+        System.out.println("Sum is : "+ (n1+n2));
+    }
+}
