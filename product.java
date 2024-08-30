3defc0ff96853e6418b46e199cd343454371f80d
dhara-maru <dharamaru406@gmail.com>
2024-08-30 19:28:32 +0530

    next

---
+++ product.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class product{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter num1: ");
+        int n1 = sc.nextInt();
+        System.out.println("enter num2: ");
+        int n2 = sc.nextInt();
+        System.out.println("Product is : "+ (n1*n2));
+    }
+}
