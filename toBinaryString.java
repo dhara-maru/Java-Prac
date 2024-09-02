57e13fab87fbf3038af2259a9169097178363624
dhara-maru <dharamaru406@gmail.com>
2024-09-02 18:00:26 +0530

    toBinarystring prg

---
+++ toBinaryString.java
@@ -0,0 +1,9 @@
+public class toBinaryString {
+    public static void main(String[] args) {
+
+        System.out.println(" 1 : " + Integer.toBinaryString(1));
+        System.out.println(" -1 : " + Integer.toBinaryString(-1));
+        System.out.println(" -1 >> 16 : " + Integer.toBinaryString(-1 >> 16));
+        System.out.println(" -1 >>> 16 : " + Integer.toBinaryString(-1 >>> 16));
+    }
+}
