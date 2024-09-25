4091aa59e1051358f5108336e01ef9491fd48796
dhara-maru <dharamaru406@gmail.com>
2024-09-25 15:54:53 +0530

    25Sept HW | Static class, block, variable, method demo

---
+++ HOMEWORK_prgs/staticdemo.java
@@ -0,0 +1,40 @@
+
+class staticcls {
+
+    static int count; //static variable
+
+    staticcls() {
+        count++;
+    }
+
+    static int getcount() {  //static method
+        return count;
+    }
+
+    static {  // Static block
+        System.out.println("Static block executed.");
+
+    }
+
+    // Static class
+    static class NestedStaticClass {
+
+        void display() {
+            System.out.println("Inside static nested class.");
+        }
+    }
+}
+
+public class staticdemo {
+
+    public static void main(String[] args) {
+        staticcls sc1 = new staticcls();
+        staticcls sc2 = new staticcls();
+        int answer = staticcls.getcount();
+        System.out.println("there are " + answer + " objects created in the class.");
+
+        staticcls.NestedStaticClass nested = new staticcls.NestedStaticClass();
+        nested.display();
+
+    }
+}
