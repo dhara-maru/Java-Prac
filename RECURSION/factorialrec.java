cffdd659264c5e7eac68d51e574539295d7609c9
dhara-maru <dharamaru406@gmail.com>
2024-09-28 12:23:24 +0530

    3. factorial using recursion

---
+++ RECURSION/factorialrec.java
@@ -0,0 +1,16 @@
+
+public class factorialrec {
+
+    public static int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n = n * factorial(n - 1);
+
+    }
+
+    public static void main(String[] args) {
+        int res = factorial(5);
+        System.out.println(res);
+    }
+}
