9e023616024eb42444db19a5e2355be1aac2561e
dhara-maru <dharamaru406@gmail.com>
2024-09-29 16:29:04 +0530

    Range Prime fixed

--- LAB3_JAVA_DHARA/prac3d.java
+++ LAB3_JAVA_DHARA/prac3d.java
@@ -1,25 +1,26 @@
+
 import java.util.*;
-class prac3d{
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Enter a number: ");
-		int n= sc.nextInt();
+class prac3d {
 
-boolean isprime=false;
-		for(int i=2; i<n-1; i++){
-			if(i%2==0){
-				isprime = false;
-			}
-			else{
-				isprime=true;
-			}
-		}
-		if(isprime){
-			System.out.println("number is prime");
-		}
-		else{
-			System.out.println("Not prime");
-		}
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+
+        boolean isprime = false;
+        for (int i = 2; i < n - 1; i++) {
+            if (n % i == 0) {
+                isprime = false;
+            } else {
+                isprime = true;
+            }
+        }
+        if (isprime) {
+            System.out.println("number is prime");
+        } else {
+            System.out.println("Not prime");
+        }
+    }
 }
--- LAB3_JAVA_DHARA/prac3g.java
+++ LAB3_JAVA_DHARA/prac3g.java
@@ -1,34 +1,30 @@
+
 import java.util.*;
-class prac3g{
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Enter starting number: ");
-		int r1= sc.nextInt();
-		System.out.println("Enter Ending number: ");
-		int r2= sc.nextInt();
+class prac3g {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter starting number: ");
+        int r1 = sc.nextInt();
+        System.out.println("Enter Ending number: ");
+        int r2 = sc.nextInt();
 
-for(int j=r1; j <r2; j++){
-boolean isprime=true;	
-		for(int i=2; i<j-1; i++){
-			if(i%2==0){
-				isprime=false;
-			}
-			else{
-				isprime=true;
-			}
-		}//prime for	
-	
-	
+        for (int i = r1; i <= r2; i++) {
+            boolean prime = true;
 
-		if(isprime){
-			System.out.println(j);
-		}
-	}//range for
-	
-	}
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+            if (prime) {
+                System.out.println(i);
+            }
+        }
 
+    }
 
 }
