7336ed6dc3c0d7e3fbf9d2392dfa24de4e4aecb8
dhara-maru <dharamaru406@gmail.com>
2024-11-03 23:26:48 +0530

    getPriority() & setPriority()

--- HOMEWORK_prgs/cwht1.java
+++ HOMEWORK_prgs/cwht1.java
@@ -1,20 +1,24 @@
 class gm extends Thread {
     public void run() {
-        while (true) {
+        int i = 0;
+        while (i <= 50) {
             System.out.println("Good morning");
+            i++;
         }
     }
 }
 
 class welcome extends Thread {
     public void run() {
-        while (true) {
+        int i = 0;
+        while (i <= 50) {
             try {
-                Thread.sleep(200);
+                // Thread.sleep(100);
             } catch (Exception e) {
                 e.printStackTrace();
             }
             System.out.println("welcome");
+            i++;
         }
     }
 }
@@ -23,7 +27,12 @@ public class cwht1 {
     public static void main(String[] args) {
         gm gm1 = new gm();
         welcome welcome1 = new welcome();
+
         gm1.start();
         welcome1.start();
+        welcome1.setPriority(Thread.MAX_PRIORITY);
+        System.out.println(gm1.getPriority());
+        System.out.println(welcome1.getPriority());
+
     }
 }
