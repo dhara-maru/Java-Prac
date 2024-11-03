c3126b82ad9cb2ccda9e90a3ab29579a72190950
dhara-maru <dharamaru406@gmail.com>
2024-11-03 23:08:30 +0530

    Thread priority : MIN, MAX, NORM

---
+++ HOMEWORK_prgs/threadprio.java
@@ -0,0 +1,32 @@
+
+
+class my1 extends Thread {
+    my1(String name) {
+        super(name);
+    }
+
+    public void run() {
+        System.out.println("name : " + this.getName());
+
+    }
+}
+
+public class threadprio {
+    public static void main(String[] args) {
+        my1 t1 = new my1("Alpha");
+        my1 t2 = new my1("Mid");
+        my1 t3 = new my1("cooked");
+        my1 t4 = new my1("fried");
+        my1 t5 = new my1("womp womp");
+
+        t1.setPriority(Thread.MAX_PRIORITY);
+        t5.setPriority(Thread.NORM_PRIORITY);
+        t2.setPriority(Thread.MIN_PRIORITY);
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+
+    }
+}
