144817a884078675bd7fe09848eb301881f863e4
dhara-maru <dharamaru406@gmail.com>
2024-11-08 21:38:39 +0530

    ArrayList

--- OOPS/arraylistdemo.java
+++ OOPS/arraylistdemo.java
@@ -5,6 +5,7 @@ public class arraylistdemo {
     public static void main(String[] args) {
         ArrayList<Integer> l1 = new ArrayList<>();
         ArrayList<Integer> l2 = new ArrayList<>(2);
+        ArrayList<String> l3 = new ArrayList<>();
 
         l2.add(00);
         l2.add(01);
@@ -18,11 +19,28 @@ public class arraylistdemo {
         l1.add(0, 20);
         l1.addAll(0, l2);
 
+        l3.add("Java");
+        l3.add("Python");
+        l3.add("Javascript");
+        l3.add("CPP");
+        l3.add("C");
+        l3.add("C#");
+        l3.add(0, "Dart"); // on 0th index
+        l3.remove(4); // removes 4th index elem
+        l3.remove("C#"); // removes C#
+        l3.set(1, "Python9");
+
         for (int i = 0; i < l1.size(); i++) {
             System.out.println(l1.get(i));
         }
 
+        for (int i = 0; i < l3.size(); i++) {
+            System.out.println(l3.get(i));
+        }
+
         System.out.println(l1.contains(7));
         System.out.println(l1.indexOf(20));
+        System.out.println(l3.isEmpty()); // false
+
     }
 }
