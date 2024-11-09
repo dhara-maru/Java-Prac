61c7e40456252e0e6d79e667bb38e709db7fd876
dhara-maru <dharamaru406@gmail.com>
2024-11-09 09:34:19 +0530

    find character case

---
+++ OOPS/ninjas1.java
@@ -0,0 +1,23 @@
+
+// find character case
+import java.util.*;
+
+class ninjas1 {
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+
+        if (c >= 65 && c <= 90) {
+            System.out.println("1");
+
+        } else if (c >= 97 && c <= 122) {
+            System.out.println("0");
+
+        } else {
+            System.out.println("-1");
+        }
+
+    }
+}
