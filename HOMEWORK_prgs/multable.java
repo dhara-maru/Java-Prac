457a7c34050995874d338c5508a1ce3a831feadd
dhara-maru <dharamaru406@gmail.com>
2024-11-28 13:13:50 +0530

    multiplication table

---
+++ HOMEWORK_prgs/multable.java
@@ -0,0 +1,10 @@
+
+public class multable {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " X " + i + " = " + (n * i));
+        }
+    }
+}
