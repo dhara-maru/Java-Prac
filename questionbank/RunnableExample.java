5ea4f2bde06a32df7fe7526119b97970613dab60
D46 <155808006+dhara-maru@users.noreply.github.com>
2024-12-06 18:23:13 +0530

    Update questionbank

---
+++ questionbank/RunnableExample.java
@@ -0,0 +1,21 @@
+public class RunnableExample implements Runnable {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(Thread.currentThread().getName() + ": " + i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        RunnableExample task = new RunnableExample();
+        Thread thread1 = new Thread(task, "Thread-1");
+        Thread thread2 = new Thread(task, "Thread-2");
+
+        thread1.start();
+        thread2.start();
+    }
+}
