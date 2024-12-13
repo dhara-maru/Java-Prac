16e588a19f465fcf2634f35d8a67d07b47416f98
dhara-maru <dharamaru406@gmail.com>
2024-12-13 18:43:32 +0530

    java paper prac

--- questionbank/replacements.java
+++ questionbank/replacements.java
@@ -1,24 +1,16 @@
-import java.io.IOException;
 import java.io.*;
 
 public class replacements {
     public static void main(String[] args) {
         try {
-            FileInputStream fin = new FileInputStream("file1.txt");
-            FileOutputStream fout = new FileOutputStream("file222.txt");
-
-            String word1 = "word1";
-            String word2 = "word2";
-
-            int i = 0;
-            byte[] b = fin.readAllBytes();
-            String content = new String(b);
-
-            while (content.contains(word1)) {
-                content = content.replaceFirst(word1, word2);
-
-            }
-            fout.write(content.getBytes());
+            FileInputStream fis = new FileInputStream("file222.txt");
+            FileOutputStream fos = new FileOutputStream("output.txt");
+            byte[] buffer = fis.readAllBytes();
+            String content = new String(buffer);
+            content = content.replace("word2", "word3");
+            fos.write(content.getBytes());
+            fis.close();
+            fos.close();
 
         } catch (IOException e) {
             e.printStackTrace();
